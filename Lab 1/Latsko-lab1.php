3763dfa54284369cad4d2de2ded114458e2bd274
StevenLatsko <36185640+StevenLatsko@users.noreply.github.com>
2020-09-08 00:07:25 -0400

    Add files via upload

---
+++ Lab 1/Latsko-lab1.php
@@ -0,0 +1,21 @@
+<?php
+
+   // PHP code to initialize variables
+   $message = "<span class='redtext'>This is PHP.</span>";
+
+?>
+<html lang="en">
+<head>
+   <style>
+      .redtext { color:blue; }
+   </style>
+   <title>Lab 1</title>
+</head>
+<body>
+   <h2>Steve Latsko - Lab 1</h2>
+   
+   <p>This is HTML</p>
+   
+   <p>The message is: <?php echo $message; ?></p>
+</body>
+</html>
